118ff7a8e5da5ef7acdd6bd4842ed6a8225623e9
Le Tuyen <letuyen@192.168.2.5>
2023-06-04 15:36:53 +0700

    delete src

--- src/screens/index.ts
+++
@@ -1,8 +0,0 @@
-import SettingView from './SettingView';
-import HomeView from './HomeView';
-import LoginView from './LoginView';
-import ListNotification from './ListNotification';
-import ListSMS from './ListSMS';
-import ListLog from './ListLog';
-
-export {SettingView, HomeView, LoginView, ListNotification, ListSMS, ListLog};
